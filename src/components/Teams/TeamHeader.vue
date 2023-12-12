b56750cebfd78b84dc788ae5d9ff30e81aa647cb
usagreatus <tolyaaaa@gmail.com>
2023-12-11 22:15:33 -0500

    Team management project on vite and vue3

---
+++ src/components/Teams/TeamHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import AddMemberModal from "@/components/Teams/AddMemberModal.vue";
+  import {useTeamStore} from "@/stores/TeamStore.js";
+
+  let team = useTeamStore();
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <div>
+      <AddMemberModal />
+    </div>
+    <div>
+      <div class="inline-flex items-center text-3xl relative">
+        <img src="/smiley.png" alt="" class="mr-2">
+        <h3>{{ team.name }} Team</h3>
+        <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">{{ team.spots }}</div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TeamMembers from "@/components/Teams/TeamMembers.vue";
+import TeamHeader from "@/components/Teams/TeamHeader.vue";
+import TeamFooter from "@/components/Teams/TeamFooter.vue";
+import {useTeamStore} from "@/stores/TeamStore.js";
+
+let team = useTeamStore();
+team.fill();
+</script>
+
+<template>
+  <TeamHeader />
+  <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
+    <TeamMembers />
+  </div>
+  <TeamFooter />
+</template>
